000466e79653be37fa6a496ca2ecb590afba286a
OWEN YATOR <yatorowen@gmail.com>
2025-01-22 08:55:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.thenewsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsprojectpractice.R
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_news)
+
+    }
+}
