ec976f4e149f534776efc00fe781d7d5ba342ca1
owen-yator <yatorowen@gmail.com>
2025-01-23 09:48:51 +0300

    This commit includes files of the models layout and Constants.kt which contains constants that will be used.

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "831039980f054b9fa3b06b6aa4d4f110"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
