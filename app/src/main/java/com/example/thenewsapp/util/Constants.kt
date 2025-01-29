b115317b366ab509697f42462bc566841ac4b120
owen-yator <yatorowen@gmail.com>
2025-01-29 22:36:50 +0300

    updated Constants.

--- app/src/main/java/com/example/thenewsapp/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "831039980f054b9fa3b06b6aa4d4f110"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
